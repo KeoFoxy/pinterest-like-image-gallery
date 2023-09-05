d21d4c2109139f6e83c1d5dfbf2cc698b89250fe
KeoFoxy <arasdiel68@gmail.com>
2023-09-05 17:11:23 +0300

    Image in card styling done

--- components/ImageCard/ImageCard.tsx
+++ components/ImageCard/ImageCard.tsx
@@ -1,5 +1,4 @@
 
-import Image from "next/image";
 import { useState, MouseEvent } from "react";
 import { ImageCardProps } from "@/components/ImageCard/ImageCard.props";
 
@@ -19,31 +18,29 @@ const ImageCard = ( {imageUrl, comment}: ImageCardProps) => {
 
     return (
         <>
-            <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden cursor-pointer" onClick={handleImageClick}>
+            <div className="w-60 mx-auto bg-white rounded-xl shadow-md overflow-hidden">
                 <div className="p-2">
                     <img
                         src={imageUrl}
                         alt="Image content"
-                        className="max-w-full align-middle rounded-md shadow-xl"
+                        className="max-w-full align-middle rounded-md shadow-xl cursor-pointer"
+                        onClick={handleImageClick}
                     />
                 </div>
                 <div className="p-4">
-                    <p className="text-lg leading-relaxed">{comment}</p>
+                    <p className="text-lg leading-relaxed text-gray-700">{comment}</p>
                 </div>
             </div>
 
             {isOpen && (
                 <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center z-50" onClick={handleClose}>
-                    <img src={imageUrl} alt="Image content" style={{
-                        border: '0',
-                        height: 'auto',
-                        maxWidth: '80%',
-                        verticalAlign: 'middle'
-                    }}/>
+                    <img src={imageUrl} alt="Image content" className="max-w-4/5 align-middle"/>
                 </div>
             )}
         </>
     );
+
+
 };
 
 export default ImageCard;
