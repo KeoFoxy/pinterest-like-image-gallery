af2fdcd708e1cd68548f97ac144765fbed4c72f8
KeoFoxy <arasdiel68@gmail.com>
2023-09-05 18:56:13 +0300

    Mock image card

---
+++ components/Gallery/Gallery.tsx
@@ -0,0 +1,13 @@
+import {ImageCard} from "@/components";
+
+const Gallery = () => {
+
+
+    return (
+        <div>
+            <ImageCard imageUrl={"https://i.pinimg.com/564x/da/82/4f/da824fc9fa228a807c812456ded8ef46.jpg"} comment={"Some comment"} />
+        </div>
+    );
+};
+
+export default Gallery;
