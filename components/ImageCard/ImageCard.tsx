1596d16d8cfd8136085c291540ccb33853bd3bb0
KeoFoxy <arasdiel68@gmail.com>
2023-09-05 18:55:20 +0300

    Image Card splited into 2 separated parts

--- components/ImageCard/ImageCard.tsx
+++ components/ImageCard/ImageCard.tsx
@@ -1,46 +1,15 @@
 
-import { useState, MouseEvent } from "react";
 import { ImageCardProps } from "@/components/ImageCard/ImageCard.props";
+import {Commentary, ImageComponent} from "@/components";
 
 const ImageCard = ( {imageUrl, comment}: ImageCardProps) => {
 
-    const [isOpen, setIsOpen] = useState(false);
-
-    const handleImageClick = () => {
-        setIsOpen(true);
-    }
-
-    const handleClose = (e: MouseEvent<HTMLDivElement>) => {
-        if (e.target === e.currentTarget) {
-            setIsOpen(false);
-        }
-    }
-
     return (
-        <>
-            <div className="w-60 mx-auto bg-white rounded-xl shadow-md overflow-hidden">
-                <div className="p-2">
-                    <img
-                        src={imageUrl}
-                        alt="Image content"
-                        className="max-w-full align-middle rounded-md shadow-xl cursor-pointer"
-                        onClick={handleImageClick}
-                    />
-                </div>
-                <div className="p-4">
-                    <p className="text-lg leading-relaxed text-gray-700">{comment}</p>
-                </div>
-            </div>
-
-            {isOpen && (
-                <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center z-50" onClick={handleClose}>
-                    <img src={imageUrl} alt="Image content" className="max-w-4/5 align-middle"/>
-                </div>
-            )}
-        </>
+        <div className="w-60 mx-auto bg-white rounded-xl shadow-md overflow-hidden">
+            <ImageComponent imageUrl={imageUrl} />
+            <Commentary text={comment} />
+        </div>
     );
-
-
 };
 
 export default ImageCard;
