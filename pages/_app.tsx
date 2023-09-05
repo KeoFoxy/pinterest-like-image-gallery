893c1835cbc28328c5e64a1d640efac2ccfbd810
KeoFoxy <arasdiel68@gmail.com>
2023-09-05 18:55:33 +0300

    Gallery

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import {Gallery} from "@/components";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <Gallery />
+      </>
+
+  )
 }
