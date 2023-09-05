a89b517f2e7efbabd4848b103b02ab12a22e5be8
KeoFoxy <arasdiel68@gmail.com>
2023-09-05 17:22:32 +0300

    Image separated as component from Card

---
+++ components/ImageComponent/ImageComponent.tsx
@@ -0,0 +1,39 @@
+import { MouseEvent, useState } from "react";
+import {ImageComponentProps} from "@/components/ImageComponent/ImageComponent.props";
+
+const ImageComponent = ({imageUrl}: ImageComponentProps) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleImageClick = () => {
+        setIsOpen(true);
+    }
+
+    const handleClose = (e: MouseEvent<HTMLDivElement>) => {
+        if (e.target === e.currentTarget) {
+            setIsOpen(false);
+        }
+    }
+
+    return (
+        <>
+            <div className="w-60 mx-auto bg-white rounded-xl shadow-md overflow-hidden">
+                <div className="p-2">
+                    <img
+                        src={imageUrl}
+                        alt="Image content"
+                        className="max-w-full align-middle rounded-md shadow-xl cursor-pointer"
+                        onClick={handleImageClick}
+                    />
+                </div>
+            </div>
+
+            {isOpen && (
+                <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center z-50" onClick={handleClose}>
+                    <img src={imageUrl} alt="Image content" className="max-w-4/5 align-middle"/>
+                </div>
+            )}
+        </>
+    );
+};
+
+export default ImageComponent;
