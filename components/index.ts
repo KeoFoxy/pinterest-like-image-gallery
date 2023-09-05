0659d36e485325545ed65c3619a8ea69636f07a4
KeoFoxy <arasdiel68@gmail.com>
2023-09-05 18:54:51 +0300

    Index new components

---
+++ components/index.ts
@@ -0,0 +1,11 @@
+import Gallery from "@/components/Gallery/Gallery";
+import ImageCard from "@/components/ImageCard/ImageCard";
+import ImageComponent from "@/components/ImageComponent/ImageComponent";
+import Commentary from "@/components/Commentary/Commentary";
+
+export {
+    Gallery,
+    ImageCard,
+    ImageComponent,
+    Commentary
+}
