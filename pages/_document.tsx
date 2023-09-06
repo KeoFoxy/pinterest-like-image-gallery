0781d38b3910291839a5205f8e63e3eb2c461edc
KeoFoxy <arasdiel68@gmail.com>
2023-09-06 20:17:57 +0300

    Fullsize for body

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className={"bg-gray-200"}>
+      <body className="min-h-screen bg-gray-200">
         <Main />
         <NextScript />
       </body>
